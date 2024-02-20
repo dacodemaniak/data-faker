9a429e2c48ed6028609ecbe1ffc27d5c4df9302f
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2024-02-20 15:42:45 +0100

    Services instanciation at bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { SelectPrompt } from './core/cli/prompts/select.prompt'
+import { BuildConfigService } from './core/build/build-config.service'
+import { LanguageSelectPrompt } from './cli/language-select-prompt'
 export class Main {
     constructor() {
         this.bootstrap()
     }
 
     private async bootstrap() {
-
+        BuildConfigService.getInstance()
     }
 }
 
